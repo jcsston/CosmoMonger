7534f9e2b223bbf5fd7c2e754045969b52b0857b
jcsston <jcsston@3afc3cd8-edf8-418b-809c-5bd5fe7563cd>
2009-01-08 13:10:49 +0000

    Adding missing file.
    git-svn-id: https://subversion.assembla.com/svn/cosmomonger/trunk@75 3afc3cd8-edf8-418b-809c-5bd5fe7563cd

---
+++ CosmoMonger/CosmoMonger/Controllers/CosmoMongerHandleErrorAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace CosmoMonger.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+    using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling.Logging;
+
+    public class CosmoMongerHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && !filterContext.ExceptionHandled && filterContext.HttpContext.IsCustomErrorEnabled)
+            {
+                Exception innerException = filterContext.Exception;
+
+                bool rethrow = ExceptionPolicy.HandleException(innerException, "Page Policy");
+                if (rethrow)
+                {
+                    throw innerException;
+                }
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
