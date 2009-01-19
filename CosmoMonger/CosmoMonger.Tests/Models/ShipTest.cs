eee9785b903198e1688609d7d4e5d99915eb3569
jcsston <jcsston@3afc3cd8-edf8-418b-809c-5bd5fe7563cd>
2009-01-19 01:00:17 +0000

    Added more tests for Ship model object. Reference #45
    git-svn-id: https://subversion.assembla.com/svn/cosmomonger/trunk@115 3afc3cd8-edf8-418b-809c-5bd5fe7563cd

--- CosmoMonger/CosmoMonger.Tests/Models/ShipTest.cs
+++ CosmoMonger/CosmoMonger.Tests/Models/ShipTest.cs
@@ -9,6 +9,7 @@
     using CosmoMonger.Models;
     using NUnit.Framework;
     using NUnit.Framework.SyntaxHelpers;
+    using Moq;
 
     /// <summary>
     /// Summary description for ShipTest
@@ -104,6 +105,71 @@
 
                 Assert.AreEqual(targetSystem, testShip.CosmoSystem, "Ship should now be in the target system");
             }
+        }
+
+        [Test]
+        public void CargoSpaceFree()
+        {
+            // Arrange
+            Ship ship = new Ship();
+            ship.BaseShip = new BaseShip();
+            ship.BaseShip.CargoSpace = 30;
+
+            ship.JumpDrive = new JumpDrive();
+            ship.JumpDrive.CargoCost = 1;
+
+            ship.Shield = new Shield();
+            ship.Shield.CargoCost = 2;
+
+            ship.Weapon = new Weapon();
+            ship.Weapon.CargoCost = 3;
+
+            // Assert
+            Assert.That(ship.CargoSpaceFree, Is.EqualTo(30-6), "Total free cargo space should equal the BaseShip amount minus 6");
+        }
+
+        [Test]
+        public void CargoSpaceTotal()
+        {
+            // Arrange
+            BaseShip baseShip = new BaseShip();
+            baseShip.CargoSpace = 30;
+            
+            Ship ship = new Ship();
+            ship.BaseShip = baseShip;
+
+            // Assert
+            Assert.That(ship.CargoSpaceTotal, Is.EqualTo(30), "Total cargo space should equal the BaseShip amount");
+        }
+
+        [Test]
+        public void TradeInValue()
+        {
+            // Arrange
+            Ship ship = new Ship();
+            ship.BaseShip = new BaseShip();
+            ship.BaseShip.BasePrice = 1000;
+            ship.CosmoSystem = new CosmoSystem();
+
+            // Assert
+            Assert.That(ship.TradeInValue, Is.EqualTo(800), "Trade in value should equal 80% of the BaseShip value amount");
+        }
+
+        [Test]
+        public void TradeInValueFromSystem()
+        {
+            // Arrange
+            Ship ship = new Ship();
+            ship.BaseShip = new BaseShip();
+            ship.BaseShip.BasePrice = 1000;
+            ship.CosmoSystem = new CosmoSystem();
+            SystemShip systemShip = new SystemShip();
+            systemShip.BaseShip = ship.BaseShip;
+            systemShip.PriceMultiplier = 2.0;
+            ship.CosmoSystem.SystemShips.Add(systemShip);
+
+            // Assert
+            Assert.That(ship.TradeInValue, Is.EqualTo(1600), "Trade in value should equal 80% of 2x BaseShip value amount");
         }
     }
 }
