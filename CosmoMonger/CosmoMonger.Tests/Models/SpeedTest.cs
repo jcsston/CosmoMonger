95a24b4094da3d8a243afc1d8631aefb5113b711
jcsston <jcsston@3afc3cd8-edf8-418b-809c-5bd5fe7563cd>
2009-02-10 18:52:39 +0000

    Added missing unit test source file.
    git-svn-id: https://subversion.assembla.com/svn/cosmomonger/trunk@209 3afc3cd8-edf8-418b-809c-5bd5fe7563cd

---
+++ CosmoMonger/CosmoMonger.Tests/Models/SpeedTest.cs
@@ -0,0 +1,109 @@
+﻿namespace CosmoMonger.Tests.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CosmoMonger.Models;
+    using Moq;
+    using NUnit.Framework;
+    using NUnit.Framework.SyntaxHelpers;
+
+    [TestFixture]
+    public class SpeedTest
+    {
+        public const int LOOP_COUNT = 10;
+
+        [Test]
+        public void LINQWhereObject()
+        {
+            CosmoMongerDbDataContext db = CosmoManager.GetDbContext();
+            Player topPlayer = db.Players.First();
+
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                IQueryable<Player> matchingPlayer = (from p in db.Players
+                                                     where p == topPlayer
+                                                     select p);
+            }
+        }
+
+        [Test]
+        public void LINQWhereObjectId()
+        {
+            CosmoMongerDbDataContext db = CosmoManager.GetDbContext();
+            Player topPlayer = db.Players.First();
+
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                IQueryable<Player> matchingPlayer = (from p in db.Players
+                                                     where p.PlayerId == topPlayer.PlayerId
+                                                     select p);
+            }
+        }
+
+        [Test]
+        public void LINQFirst()
+        {
+            CosmoMongerDbDataContext db = CosmoManager.GetDbContext();
+
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                Player topPlayer = (from p in db.Players
+                                    where p.PlayerId == 1
+                                    select p).FirstOrDefault();
+            }
+        }
+
+        [Test]
+        public void LINQSingle()
+        {
+            CosmoMongerDbDataContext db = CosmoManager.GetDbContext();
+
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                Player topPlayer = (from p in db.Players
+                                    where p.PlayerId == 1
+                                    select p).SingleOrDefault();
+            }
+        }
+
+        [Test]
+        public void LINQToArray()
+        {
+            CosmoMongerDbDataContext db = CosmoManager.GetDbContext();
+
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                Player [] players = (from p in db.Players
+                                    where p.Alive
+                                    select p).ToArray();
+            }
+        }
+
+        [Test]
+        public void LINQToList()
+        {
+            CosmoMongerDbDataContext db = CosmoManager.GetDbContext();
+
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                List<Player> players = (from p in db.Players
+                                        where p.Alive
+                                        select p).ToList();
+            }
+        }
+
+        [Test]
+        public void LINQToIEnumerable()
+        {
+            CosmoMongerDbDataContext db = CosmoManager.GetDbContext();
+
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                IEnumerable<Player> players = (from p in db.Players
+                                               where p.Alive
+                                               select p);
+            }
+        }
+    }
+}
