c3dfbe5346727dd8c6ca746db71c8e3d41fa2429
jcsston <jcsston@3afc3cd8-edf8-418b-809c-5bd5fe7563cd>
2009-02-10 02:19:04 +0000

    Moved long running Travel unit tests to separate test fixture.
    git-svn-id: https://subversion.assembla.com/svn/cosmomonger/trunk@206 3afc3cd8-edf8-418b-809c-5bd5fe7563cd

--- CosmoMonger/CosmoMonger.Tests/Models/ShipTest.cs
+++ CosmoMonger/CosmoMonger.Tests/Models/ShipTest.cs
@@ -16,94 +16,14 @@
     [TestFixture]
     public class ShipTest : BasePlayerTest
     {
-        private Random rnd = new Random();
-        private int shipTravelLoopCount = 5;
-        private int shipTravelThreadCount = 10;
-
         [Test]
-        [CategoryAttribute("LongRunning")]
-        public void Travel()
+        public void GetInRangeSystems()
         {
             Player testPlayer = this.CreateTestPlayer();
-            Ship testShip = testPlayer.Ship;
-            CosmoSystem startingSystem = testShip.CosmoSystem;
-            
-            CosmoSystem [] possibleTargetSystems = testShip.GetInRangeSystems();
-            Assert.AreNotEqual(0, possibleTargetSystems.Length, "Ship should always be within range of at least one system");
 
-            CosmoSystem targetSystem = possibleTargetSystems[0];
+            CosmoSystem [] inRangeSystems =  testPlayer.Ship.GetInRangeSystems();
 
-            DateTime startTime = DateTime.Now;
-            int timeToTravel = testShip.Travel(targetSystem);
-            DateTime endTime = DateTime.Now;
-            DateTime travelTime = startTime.AddSeconds(timeToTravel);
-
-            while (testShip.CheckIfTraveling())
-            {
-                endTime = DateTime.Now;
-                Thread.Sleep(0);
-            }
-
-            // Calcuate the difference between predicted travel time and actual travel time
-            TimeSpan travelTimeDiff = endTime - travelTime;
-            Assert.AreEqual(0.0, travelTimeDiff.TotalSeconds, 2.0, "Ship have should taken " + timeToTravel + " seconds to travel, +/-2.0 seconds.");
-
-            Assert.AreEqual(targetSystem, testShip.CosmoSystem, "Ship should now be in the target system");
-        }
-
-        [Test]
-        [CategoryAttribute("LongRunning")]
-        public void TravelRandom()
-        {
-            TravelRandom(0);
-        }
-
-        [Test]
-        [CategoryAttribute("LongRunning")]
-        public void TravelRandomThreaded()
-        {
-            Thread[] t = new Thread[this.shipTravelThreadCount];
-            for (int i = 0; i < t.Length; i++)
-            {
-                t[i] = new Thread(new ParameterizedThreadStart(this.TravelRandom));
-                t[i].Start(i);
-            }
-            for (int i = 0; i < t.Length; i++)
-            {
-                t[i].Join();
-            }
-        }
-
-        private void TravelRandom(object playerIndex)
-        {
-            Player testPlayer = this.CreateTestPlayer();
-            Ship testShip = testPlayer.Ship;
-            CosmoSystem startingSystem = testShip.CosmoSystem;
-
-            for (int i = 0; i < this.shipTravelLoopCount; i++)
-            {
-                CosmoSystem[] possibleTargetSystems = testShip.GetInRangeSystems();
-                Assert.AreNotEqual(0, possibleTargetSystems.Length, "Ship should always be within range of at least one system");
-
-                CosmoSystem targetSystem = possibleTargetSystems[this.rnd.Next(possibleTargetSystems.Length)];
-
-                DateTime startTime = DateTime.Now;
-                int timeToTravel = testShip.Travel(targetSystem);
-                DateTime endTime = DateTime.Now;
-                DateTime travelTime = startTime.AddSeconds(timeToTravel);
-
-                while (testShip.CheckIfTraveling())
-                {
-                    endTime = DateTime.Now;
-                    Thread.Sleep(0);
-                }
-
-                // Calcuate the difference between predicted travel time and actual travel time
-                TimeSpan travelTimeDiff = endTime - travelTime;
-                Assert.AreEqual(0.0, travelTimeDiff.TotalSeconds, 2.0, "Ship have should taken " + timeToTravel + " seconds to travel, +/-2.0 seconds.");
-
-                Assert.AreEqual(targetSystem, testShip.CosmoSystem, "Ship should now be in the target system");
-            }
+            Assert.That(inRangeSystems, Is.Not.CollectionContaining(testPlayer.Ship.CosmoSystem), "In range systems should not include current system");
         }
 
         [Test]
---
+++ CosmoMonger/CosmoMonger.Tests/Models/ShipTravelTest.cs
@@ -0,0 +1,105 @@
+﻿namespace CosmoMonger.Tests.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Web.Security;
+    using CosmoMonger.Models;
+    using NUnit.Framework;
+    using NUnit.Framework.SyntaxHelpers;
+
+    [TestFixture]
+    [CategoryAttribute("LongRunning")]
+    [Explicit("These tests are for testing the travel")]
+    public class ShipTravelTest : BasePlayerTest
+    {
+        private Random rnd = new Random();
+        private int shipTravelLoopCount = 5;
+        private int shipTravelThreadCount = 10;
+
+        [Test]
+        public void Travel()
+        {
+            Player testPlayer = this.CreateTestPlayer();
+            Ship testShip = testPlayer.Ship;
+            CosmoSystem startingSystem = testShip.CosmoSystem;
+
+            CosmoSystem[] possibleTargetSystems = testShip.GetInRangeSystems();
+            Assert.AreNotEqual(0, possibleTargetSystems.Length, "Ship should always be within range of at least one system");
+
+            CosmoSystem targetSystem = possibleTargetSystems[0];
+
+            DateTime startTime = DateTime.Now;
+            int timeToTravel = testShip.Travel(targetSystem);
+            DateTime endTime = DateTime.Now;
+            DateTime travelTime = startTime.AddSeconds(timeToTravel);
+
+            while (testShip.CheckIfTraveling())
+            {
+                endTime = DateTime.Now;
+                Thread.Sleep(0);
+            }
+
+            // Calcuate the difference between predicted travel time and actual travel time
+            TimeSpan travelTimeDiff = endTime - travelTime;
+            Assert.AreEqual(0.0, travelTimeDiff.TotalSeconds, 2.0, "Ship have should taken " + timeToTravel + " seconds to travel, +/-2.0 seconds.");
+
+            Assert.AreEqual(targetSystem, testShip.CosmoSystem, "Ship should now be in the target system");
+        }
+
+        [Test]
+        public void TravelRandom()
+        {
+            TravelRandom(0);
+        }
+
+        [Test]
+        public void TravelRandomThreaded()
+        {
+            Thread[] t = new Thread[this.shipTravelThreadCount];
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = new Thread(new ParameterizedThreadStart(this.TravelRandom));
+                t[i].Start(i);
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i].Join();
+            }
+        }
+
+        private void TravelRandom(object playerIndex)
+        {
+            Player testPlayer = this.CreateTestPlayer();
+            Ship testShip = testPlayer.Ship;
+            CosmoSystem startingSystem = testShip.CosmoSystem;
+
+            for (int i = 0; i < this.shipTravelLoopCount; i++)
+            {
+                CosmoSystem[] possibleTargetSystems = testShip.GetInRangeSystems();
+                Assert.AreNotEqual(0, possibleTargetSystems.Length, "Ship should always be within range of at least one system");
+
+                CosmoSystem targetSystem = possibleTargetSystems[this.rnd.Next(possibleTargetSystems.Length)];
+
+                DateTime startTime = DateTime.Now;
+                int timeToTravel = testShip.Travel(targetSystem);
+                DateTime endTime = DateTime.Now;
+                DateTime travelTime = startTime.AddSeconds(timeToTravel);
+
+                while (testShip.CheckIfTraveling())
+                {
+                    endTime = DateTime.Now;
+                    Thread.Sleep(0);
+                }
+
+                // Calcuate the difference between predicted travel time and actual travel time
+                TimeSpan travelTimeDiff = endTime - travelTime;
+                Assert.AreEqual(0.0, travelTimeDiff.TotalSeconds, 2.0, "Ship have should taken " + timeToTravel + " seconds to travel, +/-2.0 seconds.");
+
+                Assert.AreEqual(targetSystem, testShip.CosmoSystem, "Ship should now be in the target system");
+            }
+        }
+    }
+}
