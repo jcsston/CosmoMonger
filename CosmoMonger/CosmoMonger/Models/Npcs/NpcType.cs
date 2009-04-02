396d2416b679c3558b3b94d16bb9ddcb3c35d365
jcsston <jcsston@3afc3cd8-edf8-418b-809c-5bd5fe7563cd>
2009-04-02 22:13:38 +0000

    New NpcType enum file.
    git-svn-id: https://subversion.assembla.com/svn/cosmomonger/trunk@314 3afc3cd8-edf8-418b-809c-5bd5fe7563cd

---
+++ CosmoMonger/CosmoMonger/Models/Npcs/NpcType.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NpcBase.cs" company="CosmoMonger">
+//     Copyright (c) 2009 CosmoMonger. All rights reserved.
+// </copyright>
+// <author>Jory Stone</author>
+//-----------------------------------------------------------------------
+
+namespace CosmoMonger.Models.Npcs
+{
+    using System;
+
+    /// <summary>
+    /// Enumeration of the different Npc Types
+    /// </summary>
+    public enum NpcType
+    {
+        /// <summary>
+        /// Good Balancer NPC Type
+        /// </summary>
+        GoodBalancer = 1,
+
+        /// <summary>
+        /// NPC Balancer NPC Type
+        /// </summary>
+        NpcBalancer = 2,
+
+        /// <summary>
+        /// Cleaner NPC Type
+        /// </summary>
+        Cleaner = 3,
+
+        /// <summary>
+        /// Trader NPC Type
+        /// </summary>
+        Trader = 4,
+
+        /// <summary>
+        /// Pirate NPC Type
+        /// </summary>
+        Pirate = 5,
+
+        /// <summary>
+        /// Police NPC Type
+        /// </summary>
+        Police = 6
+    }
+}
